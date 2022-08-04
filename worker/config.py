ad0e6127f6cadee2ce02c5936dca289f6ab49841
신희원 <2018008595@hanyang.ac.kr>
2022-08-04 17:00:50 +0900

    add redis server

--- worker/config.py
+++ worker/config.py
@@ -1,10 +1,10 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, Field
 
 
 class CeleryWorkerSettings(BaseSettings):
     worker_name: str = "Celery Worker"
     broker_uri: str
-    backend_uri: str
+    backend_uri: str = "Auto Generate"
 
 
 class ModelSettings(BaseSettings):
@@ -13,5 +13,28 @@ class ModelSettings(BaseSettings):
     model_max_length: int = 2048
 
 
+class RedisSettings(BaseSettings):
+    redis_host: str = "localhost"
+    redis_port: int = Field(
+        default=6379,
+        ge=0,
+        le=65535,
+    )
+    redis_db: int = Field(
+        default=0,
+        ge=0,
+        le=15,
+    )
+    redis_password: str = ""
+
+
 celery_worker_settings = CeleryWorkerSettings()
 model_settings = ModelSettings()
+redis_settings = RedisSettings()
+
+celery_worker_settings.backend_uri = "redis://:{password}@{hostname}:{port}/{db}".format(
+    hostname=redis_settings.redis_host,
+    password=redis_settings.redis_password,
+    port=redis_settings.redis_port,
+    db=redis_settings.redis_db,
+)
